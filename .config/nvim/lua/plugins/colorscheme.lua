c4a60107bc78adebe14874d6c0d0cb667a9c5871
jonwensu <wens.sumande@gmail.com>
2023-07-28 23:50:32 +0800

    add nvim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  { "EdenEast/nightfox.nvim" },
+  { "andersevenrud/nordic.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordfox",
+    },
+  },
+}
